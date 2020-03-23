be4a98c91114f26cfaa3f6d830dbf402e70730ab
LuisMMMTS <up201808912@fe.up.pt>
2020-03-23 01:20:24 +0000

    started 11

--- TP3/p11.c
+++ TP3/p11.c
@@ -7,14 +7,14 @@
 int main(int argc, char *argv[], char *envp[])
 {
  pid_t pid;
- if (argc != 3) {
- printf("usage: %s dirname destination file\n",argv[0]);
- exit(1);
- }
+ char command[]='a';
+ while (command!='exit'){
+ printf("minish >");
+ 
  pid=fork();
- if (pid > 0)
- printf("My child is going to execute command \"ls -laR %s\" \n", argv[1]);
- else if (pid == 0){
+ 
+ if (pid == 0){
+     find if there is file output
  int file=open(argv[2],O_WRONLY|O_CREAT|O_TRUNC, 0777);
  dup2(file, STDOUT_FILENO);
  execlp("ls", "ls", "-laR", argv[1], NULL);
