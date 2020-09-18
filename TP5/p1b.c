f8e4632467bc949964484616962295d60169a75b
luis <up201808912@fe.up.pt>
2020-09-18 15:20:31 +0100

    final commit

--- TP5/p1b.c
+++ TP5/p1b.c
@@ -26,27 +26,27 @@ int main()
         numbers op1;
         read(pipe_descriptors[READ], &op1.x, MAX_LINE); //arbitrary size to read, could not find buffer max const
         read(pipe_descriptors[READ], &op1.y, MAX_LINE); //arbitrary size to read, could not find buffer max const
-    }
-    printf("The sum of the values is: %i\n", op1.x + op1.y);
-    printf("The difference between the values is: %i\n", abs(op1.x - op1.y));
-    printf("The product of the values is: %d\n", op1.x * op1.y);
-    if (op.y != 0)
-    {
-        printf("The sum of the values is: %d\n", op1.x / op1.y);
-    }
-    else
-    {
-        printf("Cannot divide such values!\n");
-    }
-    exit(0);
-}
 
-numbers op1;
-close(pipe_descriptors[READ]);
-printf("insert a number:\n");
-op1.x = getchar();
-printf("insert another number:\n");
-op1.y = getchar();
+        printf("The sum of the values is: %i\n", op1.x + op1.y);
+        printf("The difference between the values is: %i\n", abs(op1.x - op1.y));
+        printf("The product of the values is: %d\n", op1.x * op1.y);
+        if (op1.y != 0)
+        {
+            printf("The sum of the values is: %d\n", op1.x / op1.y);
+        }
+        else
+        {
+            printf("Cannot divide such values!\n");
+        }
+        exit(0);
+    }
 
-write(pipe_descriptors[WRITE], &numbers, sizeof numbers); //arbitrary size to read, could not find buffer max const
+    numbers op1;
+    close(pipe_descriptors[READ]);
+    printf("insert a number:\n");
+    op1.x = getchar();
+    printf("insert another number:\n");
+    op1.y = getchar();
+
+    write(pipe_descriptors[WRITE], &numbers, sizeof numbers); //arbitrary size to read, could not find buffer max const
 }
